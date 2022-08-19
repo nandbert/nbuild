7f855a27fe5b8ac4f0dd40d47a9bd436732d4fec
Norbert Stoeffler <nandbert@web.de>
2022-08-19 15:47:31 +0200

    + examples

---
+++ examples/app/simple/hello.c
@@ -0,0 +1,17 @@
+/** 
+ *
+ *
+ *  \file      hello.c
+ *  \author    Norbert Stoeffler
+ *  \date      2022-08-19
+ */
+#include	<stdio.h>
+#include	"simple/foo.h"
+
+
+int main(void)
+{
+    printf("hello world, foo=%d\n",foo());
+
+    return 0;
+}
---
+++ examples/lib/simple/foo.h
@@ -0,0 +1,13 @@
+/** 
+ *
+ *
+ *  \file      foo.h
+ *  \author    Norbert Stoeffler
+ *  \date      2022-08-19
+ */
+#ifndef __FOO_H__
+#define __FOO_H__
+
+int foo();
+
+#endif
